d6c51842b1937876d04e0d80e9854da762883918
Minjeong63 <kmj06030@naver.com>
2024-10-12 17:32:00 +0900

    Feat: 사용자 인증, 인가 설정

---
+++ src/main/java/com/minidooray/gateway/domain/CustomUserDetails.java
@@ -0,0 +1,28 @@
+package com.minidooray.gateway.domain;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final Account account;
+
+    @Override
+    public String getUsername() {
+        return account.getUserId();
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+}
